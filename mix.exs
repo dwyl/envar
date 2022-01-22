618fd844facc33e233f7c09c2f8c75a6cce2cd20
nelsonic <nelson+github@dwyl.com>
2022-01-22 10:05:20 +0000

    add package info to mix.exs

--- mix.exs
+++ mix.exs
@@ -4,10 +4,20 @@ defmodule Envar.MixProject do
   def project do
     [
       app: :envar,
+      description: "Warn enginer when required Environment Variable is not set",
       version: "1.0.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        c: :test,
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.json": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,8 +31,28 @@ defmodule Envar.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # track test coverage: https://github.com/parroty/excoveralls
+      {:excoveralls, "~> 0.13.2", only: [:test, :dev]},
+
+      # Create Documentation Hex.docs: https://hex.pm/packages/ex_doc
+      {:ex_doc, "~> 0.22.6", only: :dev},
+    ]
+  end
+
+  #  package info for publishing to Hex.pm
+  defp package() do
+    [
+      files: ~w(lib LICENSE mix.exs README.md),
+      name: "useful",
+      licenses: ["GNU GPL v2.0"],
+      maintainers: ["dwyl"],
+      links: %{"GitHub" => "https://github.com/dwyl/useful"}
+    ]
+  end
+
+  defp aliases do
+    [
+      c: ["coveralls.html"]
     ]
   end
 end
